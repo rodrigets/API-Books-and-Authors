8b6114b41ea2ffb087f632faa05b9b70552681a8
Rodrigo Santos <rodrigosferreira91@gmail.com>
2020-02-12 15:11:58 -0300

    [FIX] Add tests in routes

--- author/tests.py
+++ author/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+import json
+from .models import Author
+from rest_framework.test import APITestCase, APIClient
+from rest_framework import status
+from django.conf import settings
 
-# Create your tests here.
+client = APIClient()
+
+
+
+class AuthorTestCase(APITestCase):
+
+    def test_get_author(self):
+
+        response = client.get("/" + settings.API_BASE_PATH + "author/", format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        response_error = client.get("/" + settings.API_BASE_PATH + "author/1", format='json')
+        self.assertEqual(response_error.status_code, status.HTTP_301_MOVED_PERMANENTLY)
